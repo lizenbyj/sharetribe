d35b7e7c987d89b7db9da62217568fc379e6f34f
Mikko Koski <mikkokos@gmail.com>
2015-03-03 16:13:40 +0200

    TransactionType belongs_to TransactionProcess
    - No it doesn't! But that's just the way Rails wants it to be. TransactionType contains the transaction_process_id foreign key. This is for the future, when we may want to change the process that is associated to the transaction type.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150226131628) do
+ActiveRecord::Schema.define(:version => 20150303140556) do
 
   create_table "auth_tokens", :force => true do |t|
     t.string   "token"
@@ -858,6 +858,7 @@ ActiveRecord::Schema.define(:version => 20150226131628) do
   create_table "transaction_types", :force => true do |t|
     t.string   "type"
     t.integer  "community_id"
+    t.integer  "transaction_process_id"
     t.integer  "sort_priority"
     t.boolean  "price_field"
     t.boolean  "preauthorize_payment",       :default => false
@@ -869,6 +870,7 @@ ActiveRecord::Schema.define(:version => 20150226131628) do
   end
 
   add_index "transaction_types", ["community_id"], :name => "index_transaction_types_on_community_id"
+  add_index "transaction_types", ["transaction_process_id"], :name => "index_transaction_types_on_transaction_process_id"
   add_index "transaction_types", ["url"], :name => "index_transaction_types_on_url"
 
   create_table "transactions", :force => true do |t|
